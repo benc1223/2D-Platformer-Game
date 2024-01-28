88fc49656e585cd6d14afcaff9154aa9e8a0bc1a
Ben <bencondon1223@gmail.com>
2024-01-28 10:34:17 -0700

    fixed ui jump bug and added hud

--- scenes/main_character.gd
+++ scenes/main_character.gd
@@ -2,7 +2,7 @@ extends CharacterBody2D
 
 
 const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const JUMP_VELOCITY = -500.0
 
 @onready var sprite_2d = $Sprite2D
 # Get the gravity from the project settings to be synced with RigidBody nodes.
@@ -11,6 +11,7 @@ var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 func _physics_process(delta):
 	#Animations
+	sprite_2d.animation = "default"
 	if (velocity.x > 1 || velocity.x<-1):
 		sprite_2d.animation = "running"
 	else:
