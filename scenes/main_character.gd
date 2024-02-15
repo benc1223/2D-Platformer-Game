af4434163a0b6b3d12fdb642f494db506040d201
Ben Condon <bencondon1223@gmail.com>
2024-02-14 21:56:56 -0700

    add custom controls

--- scenes/main_character.gd
+++ scenes/main_character.gd
@@ -14,7 +14,6 @@ func _physics_process(delta):
 
 	if (velocity.x > 1 || velocity.x < -1):
 		sprite_2d.animation = "running"
-		print(velocity.x)
 	else:
 		sprite_2d.animation = "default"
 	
@@ -23,12 +22,12 @@ func _physics_process(delta):
 		velocity.y += gravity * delta
 		sprite_2d.animation = "jumping"
 	# Handle Jump.
-	if Input.is_action_just_pressed("ui_up") and is_on_floor():
+	if Input.is_action_just_pressed("Jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
+	var direction = Input.get_axis("Left", "Right")
 	if direction:
 		velocity.x = direction * SPEED
 	else:
